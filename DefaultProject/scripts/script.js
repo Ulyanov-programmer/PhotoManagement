6d6c6fbad5c7823e559878d4ccb803dfc3792d7c
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2021-08-03 15:11:50 +0400

    Fixed merge conflicts.

--- DefaultProject/scripts/script.js
+++ DefaultProject/scripts/script.js
@@ -121,27 +121,6 @@ function showOrHideSubmenu(e) {
 const activateSubmenuButton = document.getElementById('submenu-open-button');
 activateSubmenuButton.addEventListener('click', showOrHideSubmenu);
 
-<<<<<<< HEAD
-// function headerToFixed(e) {
-//     // Calculating the degree of scrolling in pixels, 
-//     // multiply the innerWindowHeight by the desired scrolling percentage as 0.percent.
-//     /* Example:
-//         25 percent of innerWindowHeight = innerWindowHeight * 0.25 
-//         5 percent of 700 = 700 * 0.05
-//     */
-//     var scrollPercentage = innerWindowHeight * 0.15;
-
-//     if (pageYOffset > scrollPercentage) {
-//         burger.classList.add('burger-black');
-//         header.classList.add('fixed-header');
-//     } else {
-//         burger.classList.remove('burger-black');
-//         header.classList.remove('fixed-header');
-//     }
-// }
-// const header = document.querySelector('.header__body');
-// window.addEventListener('scroll', headerToFixed);
-=======
 // ? Use this if you have scroll buttons.
 function scrollToElement(eventData) {
     let scrollElement = document.querySelector('.' + eventData.target.dataset.scrollTo);
@@ -176,4 +155,3 @@ function headerToFixed(e) {
 const header = document.querySelector('.header__body');
 window.addEventListener('scroll', headerToFixed);
 */
->>>>>>> DefaultTemplate
