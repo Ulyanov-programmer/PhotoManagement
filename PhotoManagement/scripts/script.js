66e356ed57801c1e3d6d1ac5bd08ef3aebfbff64
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2021-08-19 11:36:28 +0400

    Added content resizing via scroll input.
    The animation of the pop-up information windows has been changed.

--- PhotoManagement/scripts/script.js
+++ PhotoManagement/scripts/script.js
@@ -117,7 +117,9 @@ function appendInfoModalMenu(e) {
     modalElementClone.classList.remove('_non-active');
 
     targetContentPreview.append(modalElementClone);
-    modalElementClone.classList.add('_active');
+    setTimeout(function () {
+        modalElementClone.classList.add('_active');
+    }, 30)
 }
 function removeInfoModalMenu(e) {
     // Try to get modal block.
@@ -138,8 +140,39 @@ contentElements.forEach(element => {
     element.addEventListener("mouseleave", removeInfoModalMenu);
 });
 
-function changeContentWidth(inputEvent) {
-    let albumContentWidth = 260;
+function changeContentWidthByWheel(inputEvent) {
+    let elementsWasResized = albumElements[0].style.width !== "";
+    // Takes resize (scroll) value and rounding to the integer.
+    // You can change the step by changing the value * -0.01.
+    let changeSize = parseInt(inputEvent.deltaY * -0.01);
+
+    let oldContentWidth = albumContentWidth;
+    if (elementsWasResized) {
+        oldContentWidth = parseInt(albumElements[0].style.width.replace("px", ""));
+    }
+    let newContentWidth;
+
+    if (changeSize >= 0) {
+        newContentWidth = oldContentWidth + changeSize;
+    } else {
+        newContentWidth = oldContentWidth - Math.abs(changeSize);
+    }
+    // Does not allow you to change the size 
+    // if it is below the permissible values of min and max in the range input.
+    if (newContentWidth <= albumContentWidth + 102 && newContentWidth >= albumContentWidth - 102) {
+        albumElements.forEach(element => {
+            element.style.width = newContentWidth + "px";
+        });
+        // Changes the position of the slider.
+        if (changeSize <= 0) {
+            inputEvent.target.value = parseInt(inputEvent.target.value) - Math.abs(changeSize);
+        } else {
+            inputEvent.target.value = parseInt(inputEvent.target.value) + changeSize;
+        }
+    }
+}
+
+function changeContentWidthByClick(inputEvent) {
     let changeSize = parseInt(inputEvent.currentTarget.value);
     let newContentWidth;
 
@@ -154,12 +187,16 @@ function changeContentWidth(inputEvent) {
         element.style.width = newContentWidth + "px";
     });
 }
+let albumContentWidth = 260;
 const albumElements = document.querySelectorAll('.album-element');
 const rangeInput = document.querySelector('#blockSizer');
 
-rangeInput.addEventListener('input', changeContentWidth);
-
-rangeInput.oncontextmenu = function (event) {
+rangeInput.addEventListener('input', changeContentWidthByClick);
+rangeInput.onwheel = (arg) => {
+    changeContentWidthByWheel(arg);
+    return false;
+}
+rangeInput.oncontextmenu = (event) => {
     if (event.which == 3) {
         albumElements.forEach(element => {
             element.style.width = '';
